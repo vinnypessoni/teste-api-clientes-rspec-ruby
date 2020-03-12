d74c5453806fe38ca6c5a386313ed83856475099
Vinny <viniciuspessoni@gmail.com>
2020-03-12 12:04:26 +0000

    Ampliando os testes e incluindo instruções de execução

--- ExemploTesteApi_Rspec.rb
+++ ExemploTesteApi_Rspec.rb
@@ -4,16 +4,63 @@ require 'pry'
 
 class TestaCliente
 
-        @@servicoClientes = "http://localhost:8080"
+        @@baseURL = "http://localhost:8080/"
+        @@recursoCliente = "cliente"
 
-        describe "Verifica zero clientes cadastrados" do
-                it "em getClientes" do
-                        resposta = HTTParty.get(@@servicoClientes)
+        describe "Testes de API Exemplo Cliente" do
+                it "Quando eu requisitar a lista de clientes sem adicionar clientes antes, Então ela deve estar vazia" do
+                        resposta = HTTParty.get(@@baseURL)
                         puts resposta.code, resposta.headers, resposta.body
                         expect(resposta.code).to eq(200)
                         expect(resposta.body).to eq("{}")
                 end
-        end
 
+                it "Quando eu cadastrar um cliente, Então ele deve ser salvo com sucesso" do
+                        requestBody = { :nome => 'Vinny', :idade => '30', :id => '1234' }
+
+                        resposta = HTTParty.post(@@baseURL + @@recursoCliente, 
+                                :body => requestBody.to_json,
+                                :headers => { 'Content-Type' => 'application/json' })
+                                
+                        puts resposta.code, resposta.headers, resposta.body
+                        expect(resposta.code).to eq(201)      
+                        expect(resposta.body).to include(requestBody[:nome])   
+                        expect(resposta.body).to include(requestBody[:idade])     
+                        expect(resposta.body).to include(requestBody[:id])                    
+                end
+
+                it "Quando eu atualizar um cliente, Então ele deve ser atualizado com sucesso" do
+                        requestBody = { :nome => 'Mickey', :idade => '64', :id => '4545' }
+                        resposta = HTTParty.post(@@baseURL + @@recursoCliente, 
+                                :body => requestBody.to_json,
+                                :headers => { 'Content-Type' => 'application/json' })
+                                     
+                        requestBody = { :nome => 'Mickey, Mouse', :idade => '18', :id => '4545' }       
+                        resposta = HTTParty.put(@@baseURL + @@recursoCliente, 
+                                :body => requestBody.to_json,
+                                :headers => { 'Content-Type' => 'application/json' })        
+                                
+                        puts resposta.code, resposta.headers, resposta.body
+                        expect(resposta.code).to eq(200)
+                        expect(resposta.body).to include(requestBody[:nome])   
+                        expect(resposta.body).to include(requestBody[:idade])     
+                        expect(resposta.body).to include(requestBody[:id])                    
+                end
+
+                it "Quando eu deletar um cliente, Então ele deve ser removido com sucesso" do
+                        requestBody = { :nome => 'Minnie Mouse', :idade => '55', :id => '8752' }
+                        resposta = HTTParty.post(@@baseURL + @@recursoCliente, 
+                                :body => requestBody.to_json,
+                                :headers => { 'Content-Type' => 'application/json' })  
+                       
+                        resposta = HTTParty.delete(@@baseURL + @@recursoCliente +"/#{requestBody[:id]}")
+                                
+                        puts resposta.code, resposta.headers, resposta.body
+                        expect(resposta.code).to eq(200)
+                        expect(resposta.body).should_not include(requestBody[:id])
+                                
+                end
+
+        end
 end
       
---
+++ README.md
@@ -0,0 +1,20 @@
+Testes para a API de exemplo do curso de APIs do viniciuspessoni.com
+
+Essa framewok de teste foi desenvolvido usando Ruby, Rspec e HttParty.
+
+Os testes exercitam os métodos GET, POST, PUT e DELETE do micro-serviço clientes.
+
+Para executar os testes, abra a pasta dos testes no terminal e execute:
+
+    rspec .\ExemploTesteApi_Rspec.rb --format doc
+
+
+[viniciuspessoni.com/downloads]( https://www.viniciuspessoni.com/downloads)
+
+Me Segue =}
+
+😍 [YouTube]( https://www.youtube.com/c/pessonizando) 
+
+💗 [Instagram](https://www.instagram.com/pessonizando)
+
+⭐ [Telegram](https:/
